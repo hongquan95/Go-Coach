f6b05c70a2e959d0d1739679e48b843604eb5aad
hongquan95 <hongquan95.tran@gmail.com>
2019-01-29 08:20:23 +0700

    Add router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css';
-import App from './App';
+import App from './components/App';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Router path="/">
+        <div>
+            <Route path="/app" component={App}/>
+            <Route path="/signup" component={SignUp}/>
+            <Route path="/signin" component={SignIn}/>
+        </div>
+    </Router>
+    , document.getElementById('root'));
 
